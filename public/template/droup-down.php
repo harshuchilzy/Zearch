f7df6f4cd349e659314b2ed3bb976f3c61e0e247
KusalDay <106142440+KusalDay@users.noreply.github.com>
2022-10-05 17:56:40 +0530

    template changes

--- public/template/droup-down.php
+++ public/template/droup-down.php
@@ -1,11 +1,62 @@
 <section class="dayz-products-dropdown">
-    <div class="container">
-        <div class="grid grid-cols-12 gap-4">
-            <div class="dayz-search-sidebar col-span-4">
+    <div class="container bg-white">
+        <div class="grid grid-cols-2 gap-4">
+            <div class="dayz-search-sidebar">
                 Price
             </div>
             <div class="dayz-search-result">
-                Results
+                <b class="mt-5">Results:250</b>
+                <div class="grid grid-cols-3 gap-4 flex">
+                    <div class="bg-white p-3">
+                        <div class="image-area">
+                            <img src="https://itslondon.s3.amazonaws.com/p/xxl/EIN4513501.jpg" alt="">
+                        </div>
+                        <div class="details-area">
+                            <p>Dewalt DCF622N 18V XR Brushless Self-Drilling TEK Screwdriver - Body</p>
+                        </div>
+                        <div class="add-to-cart">
+                            <p><span>Price</span><b>$13.00</b></p>
+                            <button>Add to Cart</button>
+                        </div>
+                    </div>
+                    <div class="bg-white p-3">
+                        <div class="image-area">
+                            <img src="https://itslondon.s3.amazonaws.com/p/xxl/EIN4513501.jpg" alt="">
+                        </div>
+                        <div class="details-area">
+                            <p>Dewalt DCF622N 18V XR Brushless Self-Drilling TEK Screwdriver - Body</p>
+                        </div>
+                        <div class="add-to-cart">
+                            <p><span>Price</span><b>$13.00</b></p>
+                            <button>Add to Cart</button>
+                        </div>
+                    </div>
+                    <div class="bg-white p-3">
+                        <div class="image-area">
+                            <img src="https://itslondon.s3.amazonaws.com/p/xxl/EIN4513501.jpg" alt="">
+                        </div>
+                        <div class="details-area">
+                            <p>Dewalt DCF622N 18V XR Brushless Self-Drilling TEK Screwdriver - Body</p>
+                        </div>
+                        <div class="add-to-cart">
+                            <p><span>Price</span><b>$13.00</b></p>
+                            <button>Add to Cart</button>
+                        </div>
+                    </div>
+                    <div class="bg-white p-3">
+                        <div class="image-area">
+                            <img src="https://itslondon.s3.amazonaws.com/p/xxl/EIN4513501.jpg" alt="">
+                        </div>
+                        <div class="details-area">
+                            <p>Dewalt DCF622N 18V XR Brushless Self-Drilling TEK Screwdriver - Body</p>
+                        </div>
+                        <div class="add-to-cart">
+                            <p><span>Price</span><b>$13.00</b></p>
+                            <button>Add to Cart</button>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     </div>
