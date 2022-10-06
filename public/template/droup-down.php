860f30d57756dfc895d35a1072e6673aaed5bced
KusalDay <106142440+KusalDay@users.noreply.github.com>
2022-10-06 12:36:41 +0530

    template-one

--- public/template/droup-down.php
+++ public/template/droup-down.php
@@ -2,12 +2,19 @@
     <div class="container bg-white shadow-lg rounded-lg">
         <div class="flex gap-4 p-5">
             <div class="dayz-search-sidebar w-1/6 p-5">
-                <div tabindex="0" class="collapse collapse-arrow  rounded-box">
+                <div tabindex="0" class="collapse collapse-open collapse-arrow  rounded-box">
                     <div class="collapse-title text-2xl font-medium">
                       Price
                     </div>
                     <div class="collapse-content"> 
-                        <p>tabindex="0" attribute is necessary to make the div focusable</p>
+                    <input type="range" min="0" max="100" value="25" class="range" step="25" />
+                        <div class="w-full flex justify-between text-xs px-2">
+                            <span>$45</span>
+                            <span>$50</span>
+                            <span>$55</span>
+                            <span>$65</span>
+                            <span>$75</span>
+                        </div>
                     </div>
                 </div>
                 <div tabindex="0" class="collapse collapse-arrow  rounded-box">
@@ -38,7 +45,7 @@
             <div class="dayz-search-result w-5/6 p-5">
                 <div class="grid grid-cols-2 gap-4 flex">
                     <div class="count p-4">
-                        <b class="mt-5">Results:250</b>
+                        <b class="mt-5">Results:04</b>
                     </div>
                     <div class="sort p-4">
                         <select class="select select-bordered w-full max-w-xs bg-white float-right">
@@ -49,52 +56,56 @@
                     </div>
                 </div>
                 <div class="grid grid-cols-4 gap-4 flex">
-                    <div class="bg-white p-3 shadow-md m-3 rounded-md p-4">
+                    <div class="bg-white shadow-md m-3 rounded-md p-4">
                         <div class="image-area flex justify-center">
                             <img src="https://itslondon.s3.amazonaws.com/p/xxl/EIN4513501.jpg" alt="" width="50%">
                         </div>
                         <div class="details-area text-center">
-                            <p>Dewalt DCF622N 18V XR Brushless Self-Drilling TEK Screwdriver - Body</p>
+                            <h5>Dewalt DCF622N 18V</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae repellendus amet opti</p>
                             <p><span>Price</span><b>$13.00</b></p>
                         </div>
                         <div class="add-to-cart flex justify-center">
-                            <a class="btn btn-dark text-white">Add to Cart</a>
+                            <a class="btn btn-dark text-white w-full text-lg">Add to Cart</a>
                         </div>
                     </div>
-                    <div class="bg-white p-3 shadow-md m-3 rounded-md p-4">
+                    <div class="bg-white shadow-md m-3 rounded-md p-4">
                         <div class="image-area flex justify-center">
                             <img src="https://itslondon.s3.amazonaws.com/p/xxl/EIN4513501.jpg" alt="" width="50%">
                         </div>
                         <div class="details-area text-center">
-                            <p>Dewalt DCF622N 18V XR Brushless Self-Drilling TEK Screwdriver - Body</p>
+                            <h5>Dewalt DCF622N 18V</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae repellendus amet opti</p>
                             <p><span>Price</span><b>$13.00</b></p>
                         </div>
                         <div class="add-to-cart flex justify-center">
-                            <a class="btn btn-dark text-white">Add to Cart</a>
+                            <a class="btn btn-dark text-white w-full text-lg">Add to Cart</a>
                         </div>
                     </div>
-                    <div class="bg-white p-3 shadow-md m-3 rounded-md p-4">
+                    <div class="bg-white shadow-md m-3 rounded-md p-4">
                         <div class="image-area flex justify-center">
                             <img src="https://itslondon.s3.amazonaws.com/p/xxl/EIN4513501.jpg" alt="" width="50%">
                         </div>
                         <div class="details-area text-center">
-                            <p>Dewalt DCF622N 18V XR Brushless Self-Drilling TEK Screwdriver - Body</p>
+                            <h5>Dewalt DCF622N 18V</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae repellendus amet opti</p>
                             <p><span>Price</span><b>$13.00</b></p>
                         </div>
                         <div class="add-to-cart flex justify-center">
-                            <a class="btn btn-dark text-white">Add to Cart</a>
+                            <a class="btn btn-dark text-white w-full text-lg">Add to Cart</a>
                         </div>
                     </div>
-                    <div class="bg-white p-3 shadow-md m-3 rounded-md p-4">
+                    <div class="bg-white shadow-md m-3 rounded-md p-4">
                         <div class="image-area flex justify-center">
                             <img src="https://itslondon.s3.amazonaws.com/p/xxl/EIN4513501.jpg" alt="" width="50%">
                         </div>
                         <div class="details-area text-center">
-                            <p>Dewalt DCF622N 18V XR Brushless Self-Drilling TEK Screwdriver - Body</p>
+                            <h5>Dewalt DCF622N 18V</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae repellendus amet opti</p>
                             <p><span>Price</span><b>$13.00</b></p>
                         </div>
                         <div class="add-to-cart flex justify-center">
-                            <a class="btn btn-dark text-white">Add to Cart</a>
+                            <a class="btn btn-dark text-white w-full text-lg">Add to Cart</a>
                         </div>
                     </div>
                 </div>
